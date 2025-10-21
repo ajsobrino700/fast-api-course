c85990a7f2e21a2277725d772dc0b473283e8eff
ajsobrino <antonio.sobrino@feedzai.com>
2025-10-21 16:58:40 +0200

    Refactoring and connect with the database

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 from fastapi import FastAPI
-from database import engine, SessionLocal
 from todo.router import router as todo_router
 from book.router import router as book_router
-import models
 
 app = FastAPI()
-
-models.Base.metadata.create_all(bind=engine)
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 
 app.include_router(router=todo_router, prefix="/todo", tags=["Todo"])
 app.include_router(router=book_router, prefix="/book", tags=["Book"])
--- todo/router.py
+++ todo/router.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter
-
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from todo.models import Todo
+from todo.database import get_db
 
 router = APIRouter()
 
 
 @router.get("")
-async def read_all():
-
-    return "Hello world"
+async def read_all(db: Annotated[Session, Depends(get_db)]):
+    return db.query(Todo).all()
